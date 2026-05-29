aadf044acde8ea0d4a08aa23bdf59c4529008e84
ndaemy <yuyaebean@gmail.com>
2026-05-30 08:16:11 +0900

    chore(tooling): add ESLint, Prettier, EditorConfig, gitignore, nvmrc

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'poc/', '*.cjs', '*.js'],
+};
